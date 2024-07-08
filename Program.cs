962aba8e99eb0c655dc1bac643944a6cea4a6fab
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-07-07 20:42:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+List<Plants> plants =
+    [
+    new Plants()
+    {
+        Species = "Venus Fly Trap",
+        LightNeeds = 4,
+        AskingPrice = 8.99m,
+        City = "Columbia",
+        ZIP = "38401",
+        Sold = false
+    },
+    new Plants()
+    {
+        Species = "Pitcher Plants",
+        LightNeeds = 3,
+        AskingPrice = 10.99m,
+        City = "Columbia",
+        ZIP = "38401",
+        Sold = false
+    },
+    new Plants()
+    {
+        Species = "Sundew",
+        LightNeeds = 5,
+        AskingPrice = 12.99m,
+        City = "Nashvile",
+        ZIP = "54876",
+        Sold = true
+    },
+    new Plants()
+    {
+        Species = "Waterwheel plant",
+        LightNeeds = 2,
+        AskingPrice = 12.73m,
+        City = "Columbia",
+        ZIP = "38401",
+        Sold = true
+    },
+    new Plants()
+    {
+        Species = "Bladderwort",
+        LightNeeds = 5,
+        AskingPrice = 8.62m,
+        City = "Nashvile",
+        ZIP = "54876",
+        Sold = false
+    }
+    ];
