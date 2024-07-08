5b74cfa25761fc0320a6761951daccdc6d5b056e
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-07-08 01:12:52 -0500

    menu options

--- Plants.cs
+++ Plants.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
-
-     public class Plants
-    {
-        public string Species { get; set; }
-        public int LightNeeds { get; set; }
-        public decimal AskingPrice { get; set; }
-        public string City { get; set; }
-        public string ZIP { get; set; }
-        public bool Sold { get; set; }
-    }
-
+public record Plants(string Species, int LightNeeds, decimal AskingPrice, string City, string ZIP, bool Sold);
--- Program.cs
+++ Program.cs
@@ -1,89 +1,146 @@
 ﻿using System.Numerics;
 
 
-        List<Plants> plants = new List<Plants>
-        {
-            new Plants()
-            {
-                Species = "Venus Fly Trap",
-                LightNeeds = 4,
-                AskingPrice = 8.99m,
-                City = "Columbia",
-                ZIP = "38401",
-                Sold = false
-            },
-            new Plants()
-            {
-                Species = "Pitcher Plants",
-                LightNeeds = 3,
-                AskingPrice = 10.99m,
-                City = "Columbia",
-                ZIP = "38401",
-                Sold = false
-            },
-            new Plants()
-            {
-                Species = "Sundew",
-                LightNeeds = 5,
-                AskingPrice = 12.99m,
-                City = "Nashville",
-                ZIP = "54876",
-                Sold = true
-            },
-            new Plants()
-            {
-                Species = "Waterwheel plant",
-                LightNeeds = 2,
-                AskingPrice = 12.73m,
-                City = "Columbia",
-                ZIP = "38401",
-                Sold = true
-            },
-            new Plants()
-            {
-                Species = "Bladderwort",
-                LightNeeds = 5,
-                AskingPrice = 8.62m,
-                City = "Nashville",
-                ZIP = "54876",
-                Sold = false
-            }
-        };
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Plants> plants =
+        [
+            new("Venus Fly Trap", 4, 8.99m, "Columbia", "38401", false),
+            new("Pitcher Plants", 3, 10.99m, "Columbia", "38401", false),
+            new("Sundew", 5, 12.99m, "Nashville", "54876", true),
+            new("Waterwheel plant", 2, 12.73m, "Columbia", "38401", true),
+            new("Bladderwort", 5, 8.62m, "Nashville", "54876", false)
+        ];
 
         string greeting = @"Welcome to the Voracious Verde~
 Please make a selection:";
 
-string choice = null;
-while (choice != "0")
-{
-    Console.WriteLine(@$"{greeting}
+        string? choice = null;
+        while (choice != "0")
+        {
+            Console.WriteLine(@$"{greeting}
                         0. Exit
                         1. Display all plants
                         2. Post a plant to be adopted
                         3. Adopt a plant
                         4. Delist a plant");
-    choice = Console.ReadLine();
-    switch (choice)
-    {
-        case "0":
-            Console.WriteLine("Thank you for viting Voracious Verde please hit anykey to leave, and come again soon.");
-            Console.ReadKey();
-            Console.Clear();
-            break;
-        case "1":
-            Console.WriteLine("a list"); // TODO
-            break;
-        case "2":
-            Console.WriteLine("Who are you leaving with us?"); // TODO
-            break;
-        case "3":
-            Console.WriteLine("Who are you taking home with you today?"); //TODO
-            break;
-        case "4":
-            Console.WriteLine("Select a plant to remove"); //TODO
-            break;
-        default: 
-            Console.WriteLine("PLEASE MAKE A SELECTION BETWEEN 0-4");
-            break;
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "0":
+                    Console.WriteLine("Thank you for viting Voracious Verde please hit anykey to leave, and come again soon.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "1":
+                    displayAllPlants();
+                    break;
+                case "2":
+                    PostPlant(plants);
+                    break;
+                case "3":
+                    AdoptAPlant(plants);
+                    break;
+                case "4":
+                    DelistAPlant(plants);
+                    break;
+                default:
+                    Console.WriteLine("PLEASE MAKE A SELECTION BETWEEN 0-4");
+                    break;
+            }
+        }
+
+        //method used for the first selection in the menu
+        void displayAllPlants()
+        {
+            for (int i = 0; i < plants.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {plants[i].Species} in {plants[i].City} {(plants[i].Sold ? "was sold" : "is available")} for ${plants[i].AskingPrice}.");
+            }
+        }
+
+        void PostPlant(List<Plants> plants)
+        {
+            Console.WriteLine("Enter the plant's details...");
+            Console.WriteLine("Plant species:");
+            string? species = Console.ReadLine();
+
+            Console.WriteLine("How much light? enter a number 1-5. 1 minimal light 5 most light");
+            int lightNeeds;
+            while (!int.TryParse(Console.ReadLine(), out lightNeeds) || lightNeeds < 1 || lightNeeds > 5)
+            {
+                Console.WriteLine("Please only enter a value 1-5.");
+                Console.WriteLine("How much light? enter a number 1-5. 1 minimal light 5 most light");
+            }
+
+            Console.WriteLine("What is the asking price? (0.00)");
+            decimal askingPrice;
+            while (!decimal.TryParse(Console.ReadLine(), out askingPrice))
+            {
+                Console.WriteLine("that is not a valid number please enter a format of 1.23");
+                Console.WriteLine("What is the asking price? (0.00)");
+            }
+
+            Console.WriteLine("City: ");
+            string? city = Console.ReadLine();
+
+            Console.WriteLine("Zip code: ");
+            string? zipCode = Console.ReadLine();
+
+            bool sold = false;
+
+            Plants newPlant = new(species, lightNeeds, askingPrice, city, zipCode, sold);
+
+            plants.Add(newPlant);
+            Console.WriteLine("Plant added.");
+        }
+
+        void AdoptAPlant(List<Plants> plants)
+        {
+            for (int i = 0; i < plants.Count; i++)
+            {
+                if (!plants[i].Sold)
+                {
+                    Console.WriteLine($"If you'd like to adopt {plants[i].Species}, enter {i}");
+                }
+            }
+            Console.Write("Enter the plant's number you'd like to adopt: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < plants.Count)
+            {
+                plants[index] = plants[index] with { Sold = true };
+                Console.WriteLine($"You have adopted {plants[index].Species}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input or the plant is already sold.");
+            }
+        }
+        void DelistAPlant(List<Plants> plants)
+        {
+            Console.WriteLine("To remove a plant enter thier number, or enter q, to return to the main menu.");
+            for (int i = 0; i < plants.Count; i++)
+            {
+                Console.WriteLine($"For {plants[i].Species} enter {i}.");
+            }
+            string? input = Console.ReadLine();
+
+            if (input == "q")
+            {
+                return;
+            }
+
+            if (int.TryParse(input, out int index) && index >= 0 && index < plants.Count)
+            {
+                Console.WriteLine($"The plant {plants[index].Species} has been removed.");
+                plants.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+        }
     }
 }
